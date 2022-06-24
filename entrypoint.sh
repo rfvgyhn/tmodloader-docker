662fa626c27bddefeae5886d3d3df314fa322531
pauvretimo <73127408+pauvretimo@users.noreply.github.com>
2022-06-24 15:02:32 +0200

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -31,7 +31,7 @@ else
     (crontab -l 2>/dev/null; echo "$TMOD_IDLE_CHECK_INTERVAL echo \"$idleMsg\" > $pipe && handle-idle $players") | crontab -
   fi
   mkfifo $pipe
-  tmux new-session -d "$server -config config.txt | tee $pipe $players" &
+  tmux new-session -d "$server -server -config /terraria-server/config.txt | tee $pipe $players" &
   sleep 60 && /usr/sbin/crond -d 8 &
   cat $pipe &
 
